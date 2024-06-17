cde37bd1b967e6735f20888037bbd46dddee9026
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-06-17 22:24:26 +0530

    added employee model

---
+++ models/EmployeeModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const employeeScheme = mongoose.Schema({
+    id: String,
+    name: String,
+    email: String,
+    phonenumber: String,
+    dob: String,
+    address: String,
+    gender: String,
+    maritalstatus: String,
+    hrpolicy: String,
+    dojoining: String,
+    punchid: String,
+    company: String,
+    branch: String,
+    designation: String,
+    mobilenumber: String,
+    reportingperson: String,
+    department: String,
+    role: String,
+    multibranchattendance: String,
+    shiftgroup: String,
+    shift: String,
+    punchtype: String,
+    geolocation: String,
+})
+
+const EmployeeModel = mongoose.model('employees', employeeScheme)
+
+module.exports = EmployeeModel
