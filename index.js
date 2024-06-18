77de2b26bf2cbc3c2e70174f3a17cacbc1458bdf
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-06-18 22:21:32 +0530

    added multiple status updation

--- index.js
+++ index.js
@@ -31,33 +31,69 @@ app.listen(3000, () => {
   console.log("Server is Running....port : 3000");
 });
 
-app.post("/", (request, response) => {
-  const { id, password } = request.body;
+// app.post("/", (request, response) => {
+//   const { id, password } = request.body;
 
-  // const salt = bcrypt.genSaltSync(10);
-  // const hash = bcrypt.hashSync(password, salt);
-  // LoginModel.create({ ...request.body, password: hash })
-  //   .then((res) => response.json(res))
-  //   .catch((err) => response.json(err));
+//   // const salt = bcrypt.genSaltSync(10);
+//   // const hash = bcrypt.hashSync(password, salt);
+//   // LoginModel.create({ ...request.body, password: hash })
+//   //   .then((res) => response.json(res))
+//   //   .catch((err) => response.json(err));
 
-  LoginModel.findOne({ id: id })
-    .then((res) => {
-      if (bcrypt.compareSync(password, res.password)) {
-        const payload = {
-          id: id,
-        };
-        const jwtToken = jwt.sign(payload, "attendanceProject");
-        response.status(200);
-        response.send({ jwtToken, msg: "Person Present in DB" });
-        // response.send("Person Present in DB");
-        return response;
-      } else {
-        return response.status(400).send("Password is incorrect");
-      }
-    })
-    .catch((err) => {
-      response.send(err);
+//   LoginModel.findOne({ id: id })
+//     .then((res) => {
+//       if (bcrypt.compareSync(password, res.password)) {
+//         const payload = {
+//           id: id,
+//         };
+//         const jwtToken = jwt.sign(payload, "attendanceProject");
+//         response.status(200);
+//         LeaveModel.findById(id)
+//           .then((res) => response.json(res))
+//           .catch((err) => response.json(err));
+//         response.send({ jwtToken, msg: "Person Present in DB" });
+//         // response.send("Person Present in DB");
+//         return response;
+//       } else {
+//         return response.status(400).send("Password is incorrect");
+//       }
+//     })
+//     .catch((err) => {
+//       response.send(err);
+//     });
+// });
+
+app.post("/", async (req, res) => {
+  // console.log(req.body)
+  const { des, id, password } = req.body;
+
+  try {
+    const user = await LoginModel.findOne({ id: id, user: des });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Password is incorrect" });
+    }
+
+    const payload = { id: id };
+    const jwtToken = jwt.sign(payload, "attendanceProject");
+
+    const employeeData = await EmployeeModel.findOne({id: id});
+    if (!employeeData) {
+      return res.status(404).json({ msg: "Employee data not found" });
+    }
+    // console.log(employeeData)
+    res.status(200).json({
+      jwtToken,
+      msg: "Person present in DB",
+      data: employeeData,
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error", error: err.message });
+  }
 });
 
 app.post("/leaveform", (request, response) => {
@@ -75,7 +111,7 @@ app.get("/leavelist", (request, response) => {
 
 app.post("/updateleave", (request, response) => {
   const { formId, currentStatus } = request.body;
-  console.log(currentStatus);
+  // console.log(currentStatus);
   LeaveModel.findByIdAndUpdate(formId, { status: currentStatus })
     .then((res) => response.json(res))
     .catch((err) => response.status(400).send("Cannot Update Status"));
@@ -83,7 +119,7 @@ app.post("/updateleave", (request, response) => {
 
 app.post("/addemployee", (request, response) => {
   const employeeDetails = request.body;
-  console.log(employeeDetails);
+  // console.log(employeeDetails);
   EmployeeModel.create(employeeDetails)
     .then((res) => {
       const salt = bcrypt.genSaltSync(10);
