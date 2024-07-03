7cc8711e4f99f8da8090334c5f756176ab2b4d76
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-03 23:19:06 +0530

    made some changes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     "GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  // res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
