94302d2246d9cebabbce24f21de7b9589e314aa8
gajendranasokkumar <gajendranasokgayu@gmail.com>
2024-07-03 21:57:20 +0530

    made cors changes

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   next();
+// });
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
